a72f1ccc0434f2eb3da7563db6dbc03adb00871e
Ankur Saini <ankursainias@gmail.com>
2019-08-17 21:15:00 +0530

    changes

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,9 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = ENV['SHOPIFY_API_KEY']
-  config.secret = ENV['SHOPIFY_API_SECRET']
+  config.api_key = '309fd909301f565c32b9e38896ac6d5b'
+  # config.api_key = ENV['SHOPIFY_API_KEY']
+  config.secret = '8fc3ae8bba5c23e7cd0277e38c22e29d'
+  # config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
   config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
